07326c23632f4e4e2a5f21e6f65de48dae742e4f
James-Mac <james@aehr.org>
2016-05-06 13:27:25 -0600

    Shortcut l337 hacker

--- app/components/repl-input.js
+++ app/components/repl-input.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   history: Ember.A(),
+  keysDown: Ember.A(),
   historyIndex: 0,
 
   keyUp ( e ) {
+    this.get('keysDown').removeObject(e.keyCode);
+
     if ( e.keyCode === 38 ) {
       let historyIndex = this.get('historyIndex') || this.get('history.length') - 1;
 
@@ -14,6 +17,15 @@ export default Ember.Component.extend({
 
       this.set('value', this.get('history').objectAt(historyIndex));
       this.decrementProperty('historyIndex');
+    }
+  },
+
+  keyDown ( e ) {
+    const keysDown = this.get('keysDown');
+    keysDown.addObject(e.keyCode);
+
+    if ( keysDown.contains(186) && (keysDown.contains(91) || keysDown.contains(17)) ) {
+      this.get('onsubmit')('/giphy ');
     }
   },
 
